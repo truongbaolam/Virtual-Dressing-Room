3d5f576c7f363001b904754a719717e0f68d7abd
truong bao lam <truongbaolam0606@gmail.com>
2023-11-08 03:07:01 +0900

    first commit

---
+++ models/ChoseItem.js
@@ -0,0 +1,8 @@
+function ChoseItem(_id, _type, _name, _desc, _imgsrc_jpg, _imgsrc_png){
+    this.id = _id;
+    this.type = _type;
+    this.name = _name;
+    this.desc = _desc;
+    this.imgsrc_jpg = _imgsrc_jpg;
+    this.imgsrc_png = _imgsrc_png;
+}
